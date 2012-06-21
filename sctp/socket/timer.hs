f74998845f1e1f47278ec9474035ee75d1804e30
Tinco Andringa <mail@tinco.nl>
2012-06-21 02:03:36 +0200

    stopping the timer

--- sctp/socket/timer.hs
+++ sctp/socket/timer.hs
@@ -4,7 +4,7 @@ import Control.Concurrent
 import Control.Concurrent.MVar
 import System.CPUTime
 
-type Timer k = MVar [(k,TimerEntry)]
+type Timer k = MVar ([(k,TimerEntry)],Bool)
 
 data TimerEntry = TimerEntry {
   timerTime :: Integer,
@@ -14,9 +14,9 @@ data TimerEntry = TimerEntry {
 -- startTimer runs a loop in which it checks its timers list every 'interval' milliseconds
 startTimer :: Int -> IO(Timer k)
 startTimer interval = do
-    mvar <- newMVar []
-    forkIO $ forever $ do
-        timers <- takeMVar mvar
+    mvar <- newMVar ([],True)
+    let timerLoop = do
+        (timers,running) <- takeMVar mvar
         time' <- getCPUTime
         let time = toMilliseconds time'
         let checkTime timer entry@(_, e) = do
@@ -27,28 +27,37 @@ startTimer interval = do
               else do
                   return $ entry : timer
         timers' <- foldM checkTime [] timers
-        putMVar mvar timers'
+        putMVar mvar (timers',running)
         threadDelay $ interval * 1000
+        when running timerLoop
+    forkIO timerLoop
     return mvar
 
 toMilliseconds time = time `div` (10^9)
+
+-- stops the timer
+stopTimer :: Timer k -> IO()
+stopTimer timerVar = do
+    (timers,_) <- takeMVar timerVar
+    putMVar timerVar (timers,False)
+    
             
 -- registerTimer adds a callback to the timers list, the callback will be called after dt milliseconds
 registerTimer :: Timer k -> k -> IO() -> Integer -> IO()
 registerTimer timerVar id callback dt = do
-    timers <- takeMVar timerVar
+    (timers,r) <- takeMVar timerVar
     time' <- getCPUTime
     let time = toMilliseconds time'
     let entry = TimerEntry (time+dt) callback
     let timers' = (id,entry) : timers
-    putMVar timerVar timers'
+    putMVar timerVar (timers',r)
 
 -- execution of the given timer will be cancelled
 cancelTimer :: Eq k => Timer k -> k -> IO()
 cancelTimer timerVar id = do
-    timers <- takeMVar timerVar
+    (timers,r) <- takeMVar timerVar
     let timers' = filter (\ (id',_) -> id /= id') timers
-    putMVar timerVar timers'
+    putMVar timerVar (timers',r)
 
 {- What we want to do is:
   - Send a packet and see if a response is received in time
